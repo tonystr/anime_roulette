50c379dfd3098d7e5a8c903f25fb271c0a4310ce
TonyStr <tony.stroemsnaes@gmail.com>
2021-02-13 17:23:36 +0100

    Abstract out signIn

--- src/components/PageRenderer.jsx
+++ src/components/PageRenderer.jsx
@@ -5,9 +5,7 @@ import ManageWheels from './ManageWheels';
 import AccessRequests from './AccessRequests';
 import WheelPage from './WheelPage';
 import ManageWheel from './ManageWheel';
-// Icons
-import FacebookLogo from '../icons/facebook_logo.png';
-import GoogleLogo from '../icons/google_logo.png';
+import SignIn from './SignIn';
 
 export default function PageRenderer() {
     const noWheelName = 'Select wheel';
@@ -94,31 +92,6 @@ export default function PageRenderer() {
         </div>
     );
 };
-
-function SignIn() {
-    // Garlic is good fzop
-    return (
-        <div className='sign-in-panel'>
-            <h2>Sign in with</h2>
-
-            <button className='sign-in google' onClick={() => {
-                const provider = new firebase.auth.GoogleAuthProvider();
-                auth.signInWithRedirect(provider);
-            }}>
-                <img alt='Google Logo' className='google-logo' src={GoogleLogo} width={50} height={50} />
-                Google
-            </button>
-
-            <button className='sign-in facebook' onClick={() => {
-                const provider = new firebase.auth.FacebookAuthProvider();
-                auth.signInWithRedirect(provider);
-            }}>
-                <img alt='Facebook Logo' className='facebook-logo' src={FacebookLogo} width={50} height={50} />
-                Facebook
-            </button>
-        </div>
-    );
-}
 
 function SignOut({ className='', ...props }) {
     return auth.currentUser ? (
---
+++ src/components/SignIn.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { firebase, auth } from '../firestore';
+// Icons
+import FacebookLogo from '../icons/facebook_logo.png';
+import GoogleLogo from '../icons/google_logo.png';
+
+export default function SignIn() {
+    return (
+        <div className='sign-in-panel'>
+            <h2>Sign in with</h2>
+
+            <button className='sign-in google' onClick={() => {
+                const provider = new firebase.auth.GoogleAuthProvider();
+                auth.signInWithRedirect(provider);
+            }}>
+                <img alt='Google Logo' className='google-logo' src={GoogleLogo} width={50} height={50} />
+                Google
+            </button>
+
+            <button className='sign-in facebook' onClick={() => {
+                const provider = new firebase.auth.FacebookAuthProvider();
+                auth.signInWithRedirect(provider);
+            }}>
+                <img alt='Facebook Logo' className='facebook-logo' src={FacebookLogo} width={50} height={50} />
+                Facebook
+            </button>
+        </div>
+    );
+};
