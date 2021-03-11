97a78e3f12d032ee3be03e309154f82634989ba5
TonyStr <tony.stroemsnaes@gmail.com>
2021-03-11 09:47:02 +0100

    revert changes

--- src/scripts/confirmAction.js
+++ src/scripts/confirmAction.js
@@ -2,6 +2,9 @@
 export default function confirmAction(query) {
     return new Promise((resolve, reject) => {
         const output = window.confirm(query);
+        // shit idk, think I would need to use context.consumer/provider
+        // everywhere I want to use this function, and that just about renders
+        // it useless man
         if (output === true || output === false) {
             return resolve(output);
         } else {
